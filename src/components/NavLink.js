bf944de8ef4234481268510eaaf2f3b4c6057199
tsuifei <tsuifei@gmail.com>
2022-08-24 20:21:08 +0200

    [Add] nav class

--- src/components/NavLink.js
+++ src/components/NavLink.js
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 const NavLink = () => {
   return (
     <div className="bg-cyan-400 p-16">
-      <nav>
-        <Link to="/app">App</Link>
-        <Link to="/Counter">Counter</Link>
-        <Link to="/Memo">Memo</Link>
+      <nav class="flex-1 px-2 pb-4 space-y-1">
+        <Link to="/app" class="nav-item">App</Link>
+        <Link to="/Counter" class="nav-item">Counter</Link>
+        <Link to="/Memo" class="nav-item">Memo</Link>
       </nav>
     </div>
   );
